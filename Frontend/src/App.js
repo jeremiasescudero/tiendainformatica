612493cd063b72387e1ca80ef35c28fc157754f4
deLlamas <87360@sistemas.frc.utn.edu.ar>
2024-06-28 18:50:11 -0300

    front

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,15 +4,8 @@ import  Inicio  from "./components/Inicio";
 import { Celulares } from "./components/celulares/Celulares";
 import { Menu } from "./components/Menu";
 import { Footer } from "./components/Footer";
-import { Notebooks } from "./components/notebooks/Notebooks";
 import { ModalDialog } from "./components/ModalDialog";
-import { Perifericos } from "./components/perifericos/Perifericos";
-import {Servicios} from "./components/servicios/Servicios" ;
-import { Login } from "./components/login/Login";
-import { MarcaCelular } from "./components/celulares/MarcaCelular";
-import { TipoPerifericos} from "./components/perifericos/TipoPerifericos";
-import {TipoServicios} from "./components/servicios/TipoServicios"
-import { MarcaNotebooks } from "./components/notebooks/MarcaNotebooks";
+
 
 
 
@@ -26,14 +19,6 @@ function App() {
         <Routes>
           <Route path="/inicio" element={<Inicio />} />
           <Route path="/celulares" element={<Celulares />} />
-          <Route path="/marcascelulares" element={<MarcaCelular />} />
-          <Route path="/notebooks" element={<Notebooks />} />
-          <Route path="/marcasnotebooks" element={<MarcaNotebooks />} />
-          <Route path="/perifericos" element={<Perifericos />} />
-          <Route path="/tipoperifericos" element={<TipoPerifericos />} />
-          <Route path="/servicios" element={<Servicios />} />
-          <Route path="/tiposervicios" element={<TipoServicios />} />
-          <Route path="/" element={<Perifericos />} />
           <Route path="*" element={<Navigate to="/inicio" replace />} />
         </Routes>
         </div>
