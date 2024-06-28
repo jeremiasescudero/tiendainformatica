134ed3c5297f8265b53a6f16451218062eb64a22
deLlamas <87360@sistemas.frc.utn.edu.ar>
2024-06-28 18:56:52 -0300

    ffront act

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,12 +4,8 @@ import { Inicio } from "./src/components/Inicio.jsx";
 import { Celulares } from "./src/components/celulares/Celulares";
 import { Menu } from "./src/components/Menu";
 import { Footer } from "./src/components/Footer";
-import { Notebooks } from "./src/components/notebooks/Notebooks";
 import { ModalDialog } from "./src/components/ModalDialog";
-import { Perifericos } from "./src/components/perifericos/Perifericos";
-import {Servicios} from "./src/components/servicios/Servicios" ;
-import { Login } from "./components/login/Login";
-import { MarcaCelular, MarcaNotebooks } from "../Backend/base-orm/sequelize-init";
+import { MarcaCelular } from "../Backend/base-orm/sequelize-init";
 
 
 
@@ -23,13 +19,6 @@ function App() {
 <Routes>
   <Route path="/inicio" element={<Inicio />} />
   <Route path="/celulares" element={<Celulares />} />
-  <Route path="/marcascelulares" element={<MarcaCelular />} />
-  <Route path="/notebooks" element={<Notebooks />} />
-  <Route path="/marcasnotebooks" element={<MarcaNotebooks />} />
-  <Route path="/perifericos" element={<Perifericos />} />
-  <Route path="/tipo" element={<Celulares />} />
-  <Route path="/servicios" element={<Servicios />} />
-  <Route path="/" element={<Perifericos />} />
   <Route path="*" element={<Navigate to="/inicio" replace />} />
 </Routes>
         </div>
