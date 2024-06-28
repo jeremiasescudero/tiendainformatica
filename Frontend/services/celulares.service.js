98d4c60528382647f4edbbd2d9f68661be9344e7
Francisco <Franciscocalvi82@gmail.com>
2024-06-28 14:05:41 -0300

    Commitfran3

---
+++ Frontend/services/celulares.service.js
@@ -0,0 +1,35 @@
+import httpService from "./http.service";
+const urlResource = "http://localhost:4000/api/celulares";
+
+
+async function Buscar(nombre) {
+    const resp = await httpService.get(urlResource, {
+        params: { nombre },
+    });
+    return resp.data;
+}
+
+
+async function BuscarPorId(item) {
+    const resp = await httpService.get(urlResource + "/" + item.IdCelulares);
+    return resp.data;
+}
+
+
+async function ActivarDesactivar(item) {
+    await httpService.delete(urlResource + "/" + item.IdCelulares);
+}
+
+
+async function Grabar(item) {
+    if (item.IdCelulares === 0) {
+        await httpService.post(urlResource, item);
+    } else {
+    await httpService.put(urlResource + "/" + item.IdCelulares, item);
+    }
+}
+
+
+export const celularesService = {
+    Buscar,BuscarPorId,ActivarDesactivar,Grabar
+};
---
+++ Frontend/services/perifericos.service.js
@@ -0,0 +1,35 @@
+import httpService from "./http.service";
+const urlResource = "http://localhost:4000/api/perifericos";
+
+
+async function Buscar(nombre) {
+    const resp = await httpService.get(urlResource, {
+        params: { nombre },
+    });
+    return resp.data;
+}
+
+
+async function BuscarPorId(item) {
+    const resp = await httpService.get(urlResource + "/" + item.IdPerifericos);
+    return resp.data;
+}
+
+
+async function ActivarDesactivar(item) {
+    await httpService.delete(urlResource + "/" + item.IdPerifericos);
+}
+
+
+async function Grabar(item) {
+    if (item.IdPerifericos === 0) {
+        await httpService.post(urlResource, item);
+    } else {
+    await httpService.put(urlResource + "/" + item.IdPerifericos, item);
+    }
+}
+
+
+export const perifericosService = {
+    Buscar,BuscarPorId,ActivarDesactivar,Grabar
+};
